6596c3b4e76f2254150e35c7ee09514f32997baa
filip <filip@smartiot.cloud>
2025-01-21 10:47:52 +0200

    added gitignore file, dotenv, webhook endpoint

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
