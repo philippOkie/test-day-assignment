7e65da4bc5bf76e15936b3251de145933bc685b4
filip <filip@smartiot.cloud>
2025-01-21 11:28:12 +0200

    added res status

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const dotenv = require("dotenv");
+
 const PORT = 3000;
 
 app.use(express.json());
@@ -11,7 +13,10 @@ app.get("/", (req, res) => {
 
 app.post("/webhook", (req, res) => {
   console.log(req.body);
+  res.status(200).end();
 });
+
+app.webhooks.on("pull_request.opened", handlePullRequestOpened);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
